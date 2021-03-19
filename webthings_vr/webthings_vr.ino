1f50a4441381c0f614ac6e9ed44cfc3ba9b41861
Vishnu Ram <vishnu.ganesan1329@gmail.com>
2021-03-19 16:54:43 +0530

    optimised the code

--- webthings_vr/webthings_vr.ino
+++ webthings_vr/webthings_vr.ino
@@ -1,23 +1,24 @@
-#include <Arduino.h>
 #include <Thing.h>
 #include <WebThingAdapter.h>
 
-
 #define BUILT_IN_LED   2
-#define PIN            34
+#define SWITCH_PIN     34
 #define LED_PIN        33
-#define BUTTON         25
 
-int flag_previous_I = 0;
-int flag_previous_II = 1;
+typedef struct
+{
+  bool flag1;
+  bool flag2;
+} switchFlag_t;
 
+switchFlag_t SW1 = {0, 1};
 
 
 /*
    SSID and Password
 */
-const char *ssid = "VR";
-const char *password = "wpa2 psk";
+const char *ssid = "SSID";
+const char *password = "PSWD";
 
 
 /*
@@ -76,44 +77,51 @@ bool lastOn = false;       // setting the value as 0
 
 
 
+void setup() {
 
-/*
-   WiFi setup
-*/
-void WiFiSetup()
-{
-  int interval = 1000;
-  int previousMillis = 0;
+
+  pinMode(BUILT_IN_LED, OUTPUT);
+  //  digitalWrite(BUILT_IN_LED, HIGH);
+
+  //user defined pins
+  pinMode(SWITCH_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, HIGH);
+
+
+  //Setting up serial
+  Serial.begin(115200);
+
+  Serial.print("Connecting to");
+  Serial.print(ssid);
+
+#if defined(ESP8266) || defined(ESP32)
+  WiFi.mode(WIFI_STA);
+#endif
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+
+  /*
+     Waiting for connection
+  */
   bool blink = true;
-  Serial.println("Waiting for WiFi");
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
     Serial.print(".");
-    digitalWrite(BUILT_IN_LED, blink ? LOW : HIGH);     //Blink until the WiFi get connected
+    //    digitalWrite(BUILT_IN_LED, blink ? LOW : HIGH);
     blink = !blink;
-    if (WiFi.status() == WL_CONNECTED)
-    {
-      break;                                            // if WiFi got connected then Break
-    }
-    unsigned long currentMillis = millis();
-    if (currentMillis - previousMillis >= interval)
-    {
-      previousMillis = currentMillis;
-      Call_ManualControl();                             // if not connected then call Manual Control
-    }
-
   }
-  digitalWrite(BUILT_IN_LED, HIGH);
-  AdapterSetup();
-}
+  //  digitalWrite(BUILT_IN_LED, HIGH);
+
+  Serial.println("");
+  Serial.print("Connected!");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 
 
-/*
-   Adapter Setup
-*/
-void AdapterSetup()
-{
   Serial.println("");
   Serial.print("Connected!");
   Serial.println(ssid);
@@ -156,156 +164,37 @@ void AdapterSetup()
   Serial.println(led.id);
 
 
-}
-
-
-void setup()
-{
-  pinMode(BUILT_IN_LED, OUTPUT);
-  digitalWrite(BUILT_IN_LED, HIGH);
-
-  pinMode(BUTTON, INPUT);
-  digitalWrite(BUTTON, HIGH);
-
-
-
-  //user defined pins
-  pinMode(PIN, INPUT);
-  pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, HIGH);
-
-  //Setting up serial
-  Serial.begin(115200);
-
-  Serial.print("Connecting to");
-  Serial.print(ssid);
-
-#if defined(ESP8266) || defined(ESP32)
-  WiFi.mode(WIFI_STA);
-#endif
-  WiFi.begin(ssid, password);
-  Serial.println("");
-
-
-  //Setting up WiFi
-  WiFiSetup();
-
 
 }
 
-
-void Adapter()
-{
+void loop() {
   ThingPropertyValue tpVal;
   bool pin;
 
-  // Reading the values
-  pin = digitalRead(PIN);
-  //Serial.println(pin);
-
-  if ( pin == LOW)
-  {
-    tpVal.boolean = pin;
-    if ( flag_previous_I == 0)
-    {
-
-      ledOn.setValue(tpVal);
-
-      bool On1 = ledOn.getValue().boolean;
-      digitalWrite(LED_PIN, On1 ? LOW : HIGH);
-
-      if (On1 != lastOn)
-      {
-        Serial.print(led.id);
-        Serial.print(": ");
-        Serial.println(On1);
-        Serial.println("condition 1");
-      }
-      lastOn = On1;
-      flag_previous_I = 1;
-      flag_previous_II = 1;
-    }
-
-  }
-
-  if ( pin == HIGH)
-  {
-    tpVal.boolean = pin;
-    if ( flag_previous_II == 1)
-    {
-
-      ledOn.setValue(tpVal);
-
-      bool On1 = ledOn.getValue().boolean;
-      digitalWrite(LED_PIN, On1 ? LOW : HIGH);
-
-      if (On1 != lastOn)
-      {
-        Serial.print(led.id);
-        Serial.print(": ");
-        Serial.println(On1);
-        Serial.println("condition 2");
-      }
-      lastOn = On1;
-      flag_previous_I = 0;
-      flag_previous_II = 0;
-    }
-
-  }
-  bool On2 = ledOn.getValue().boolean;
-  digitalWrite(LED_PIN, On2 ? LOW : HIGH);
-
-  if (On2 != lastOn)
-  {
-    Serial.print(led.id);
-    Serial.print(": ");
-    Serial.println(On2);
-    Serial.println("condition 3");
-  }
-  lastOn = On2;
-  Serial.println(lastOn);
-}
-
-
-void loop()
-{
-
-  if (WiFi.status() != WL_CONNECTED)
-  {
-    Call_ManualControl();
-    if (digitalRead(BUTTON) == LOW)
-    {
-      Serial.println("restarting ESP");
-      ESP.restart();
-    }
-  }
-
-  Adapter();
-  
-
-
-
-
-
-  // Update all the properties and events
   adapter->update();
 
-  // Wait for some time
-  delay(2000);
+  pin = digitalRead(SWITCH_PIN);
+  tpVal.boolean = pin;
 
-}
+  if (pin == LOW)
+  {
+    if (SW1.flag1 == LOW)
+    {
+      ledOn.setValue(tpVal);
+      SW1.flag1 = 1;
+      SW1.flag2 = 1;
+    }
+  }
+  if (pin == HIGH)
+  {
+    if (SW1.flag2 == HIGH)
+    {
+      ledOn.setValue(tpVal);
+      SW1.flag1 = 0;
+      SW1.flag2 = 0;
+    }
+  }
 
-
-
-/*
-    Function for manual control
-    Incase of WiFi network got disconnected(not present), this can be used to control manually
-*/
-void Call_ManualControl()
-{
-  Serial.println("Manual Control");
-  digitalWrite(LED_PIN, digitalRead(PIN));
-  Serial.println("LED");
-  Serial.println(digitalRead(PIN));
-  delay(1);
+  bool isOn = ledOn.getValue().boolean;
+  digitalWrite(BUILT_IN_LED, isOn ? HIGH : LOW);
 }
